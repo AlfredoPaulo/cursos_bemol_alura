3573c6825c208c97761f1868ced669923f1be285
Alfredo Palo <alfredopaulo2003@.com>
2022-08-22 11:55:37 -0400

    outro curso de js

---
+++ JavaScript - tipos variaveis e funcoes/index.js
@@ -0,0 +1,25 @@
+//tipo Number
+
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+console.log(operacaoMatematica)
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = numeroPontoFlutuante / pontoFlutuanteSemZero;
+
+console.log(novaOperacao)
+
+// NaN -> not A Number (não é um número )
+
+const suna =  "suna";
+
+console.log(suna * primeiroNumero)
