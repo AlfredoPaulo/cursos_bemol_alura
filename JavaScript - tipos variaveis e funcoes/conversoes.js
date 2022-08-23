8dfa8257dfe57dc877ac4bac1f7fd0dec4e36c47
Alfredo Palo <alfredopaulo2003@.com>
2022-08-23 10:35:35 -0400

    +: conversoes , falsy truthy, var let const

---
+++ JavaScript - tipos variaveis e funcoes/conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado 
+// booleanos 
+
+
+//conversão implicita
+const numero = 456
+const numeroString = '456'
+//console.log(numero + numeroString)
+
+
+//Number()
+//String()
+console.log(numero + Number(numeroString) )
+
+
+
+
+
+//conversão explicita
