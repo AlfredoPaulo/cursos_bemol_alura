2d465fec09294954293bb472e29bc0d994a2e523
Alfredo Palo <alfredopaulo2003@.com>
2022-08-23 11:56:52 -0400

    +: comparações e explicações de erros no console

---
+++ JavaScript - tipos variaveis e funcoes/comparacoes.js
@@ -0,0 +1,19 @@
+// == comparação implicita 
+
+const numero = 5;
+const texto = '5'
+
+console.log(numero === texto)
+
+//typeof
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == só comparar o valor
+// === compara o valor e o tipo de dado
+
+//conversãoo explicita
+
+Number()
+String()
---
+++ JavaScript - tipos variaveis e funcoes/tipagemdinamica.js
@@ -0,0 +1,22 @@
+//tipagem dinamica
+// untyped
+
+
+let minhaVar = 567;
+minhaVar = 'texto';
+minhaVar = true;
+
+//Multiparadigma 
+//resolver um problema de difentes maneiras
+
+
+//ES6 ? / ES2015 
+// versão do js que saiu em 2015 , versão mais famosa
+
+//Linguagem interpretada ? 
+
+// interpretada: escrever codigo e executa codigo , compiladas: compilador para codigo de maquina executa diretamente na maquina --> rodam mais rapidas
+
+
+//O que é o NodeJS ? 
+//NodeJS é um interpretador , JS no back end, opcao de console
