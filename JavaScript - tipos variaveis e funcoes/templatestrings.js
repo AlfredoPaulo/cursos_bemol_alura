e3b206418424c541e76f31c88947cc07e667dd7e
Alfredo Palo <alfredopaulo2003@.com>
2022-08-24 15:19:47 -0400

    +: ternario , template Strings

---
+++ JavaScript - tipos variaveis e funcoes/templatestrings.js
@@ -0,0 +1,9 @@
+const nome = "suna";
+const idade = 2022 - 2002
+const cidadeDeNascimento = "Manaus"
+//const apresentacao = "meu nome é " + nome;
+
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao)
