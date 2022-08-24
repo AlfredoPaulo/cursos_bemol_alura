dee1b140cefc7ccc3d758acd140f7fafcf607ea2
Alfredo Palo <alfredopaulo2003@.com>
2022-08-24 17:52:40 -0400

    +: funções em JS

---
+++ JavaScript - tipos variaveis e funcoes/funcoes.js
@@ -0,0 +1,21 @@
+// let x = "";
+
+// console.log(x)
+// x = "oi";
+
+
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+imprimeTexto("Olá , mundo !")
+
+//três formas de escrever funções
+
+
+function soma(){
+    return 2 + 2;
+
+}
+
+imprimeTexto(soma())
