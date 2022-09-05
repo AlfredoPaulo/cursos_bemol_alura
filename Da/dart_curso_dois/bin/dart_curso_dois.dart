9085e46fd104c23646a0839c55bc9240bb05790a
Alfredo Paulo Oliveira Barros <alfredopaulo2003@gmail.com>
2022-09-05 18:23:15 -0300

    + mais modificações depois da aula 1

--- Da/dart_curso_dois/bin/dart_curso_dois.dart
+++ Da/dart_curso_dois/bin/dart_curso_dois.dart
@@ -1,3 +1,17 @@
 void main() {
-  print("Olá mundo !");
+  // print("Olá mundo !1");
+
+  String nome = "Alfredo";
+  double peso = 70.0;
+  String cor = "Pardo";
+  String suna = "suna";
+  int idade = 20;
+  bool isLouco;
+
+  if(idade>= 18){
+    isLouco = true;
+  }else{
+    isLouco = false;
+  }
+
 }
